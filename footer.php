5a814ca47e114cf3aa47fa57f2aaedda0add90ab
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2022-02-23 17:35:35 -0300

    Criado footer

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
+<footer>
+    <p class="container-alura">
+        &copy; <?= date("Y") ?> - Todos os direitos reservados a Alura Intercambio
+    </p>
+</footer>
+
 </body>
 <?php wp_footer(); ?>
 </html>
